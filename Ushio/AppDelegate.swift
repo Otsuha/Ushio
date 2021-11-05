d65a2b46b4bc49583b683d353453f869b18e7035
Saiha <grass.usagi@outlook.com>
2021-11-05 19:16:53 +0800

    Ushio

--- Ushio/AppDelegate.swift
+++ Ushio/AppDelegate.swift
@@ -14,6 +14,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var iCloudURL: URL?
+        DispatchQueue.global().async {
+            while iCloudURL == nil {
+                if let url = FileManager.default.url(forUbiquityContainerIdentifier: nil) {
+                    iCloudURL = url
+                }
+            }
+        }
+        
         return true
     }
 
